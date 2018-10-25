bda6865e533e7d177d25bcc4259a6d2d73131f05
Gina <sprint@gonzaga.edu>
2018-10-24 20:34:46 -0700

    Set up base project with no data persistence

---
+++ Core Data Fun/CategoryViewController.swift
@@ -0,0 +1,86 @@
+//
+//  ViewController.swift
+//  Core Data Fun
+//
+//  Created by Gina Sprint on 10/24/18.
+//  Copyright © 2018 Gina Sprint. All rights reserved.
+//
+
+import UIKit
+
+class CategoryViewController: UITableViewController {
+    
+    var categoryArray = ["Home", "Work", "Family"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.navigationItem.leftBarButtonItem = self.editButtonItem
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return categoryArray.count
+        }
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        let category = categoryArray[indexPath.row]
+        cell.textLabel?.text = category
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            categoryArray.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let category = categoryArray.remove(at: sourceIndexPath.row)
+        categoryArray.insert(category, at: destinationIndexPath.row)
+        tableView.reloadData()
+    }
+
+    
+    @IBAction func addBarButtonPressed(_ sender: UIBarButtonItem) {
+        var alertTextField = UITextField()
+        let alert = UIAlertController(title: "Create New Category", message: "", preferredStyle: .alert)
+        
+        alert.addTextField { (textField) in
+            textField.placeholder = "Name of Category"
+            alertTextField = textField
+        }
+        
+        let action = UIAlertAction(title: "Create", style: .default) { (alertAction) in
+            let text = alertTextField.text!
+            self.categoryArray.append(text)
+            self.tableView.reloadData()
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let identifier = segue.identifier, identifier == "ShowItemsSegue"  {
+            
+            guard let itemsTableVC = segue.destination as? ItemsTableViewController else {
+                return
+            }
+        
+            guard let selectedIndexPath = tableView.indexPathForSelectedRow else {
+                return
+            }
+            
+            let category = categoryArray[selectedIndexPath.row]
+            itemsTableVC.category = category
+        }
+    }
+}
+
---
+++ Core Data Fun/ItemsTableViewController.swift
@@ -0,0 +1,88 @@
+//
+//  ItemsTableViewController.swift
+//  Core Data Fun
+//
+//  Created by Gina Sprint on 10/24/18.
+//  Copyright © 2018 Gina Sprint. All rights reserved.
+//
+
+import UIKit
+
+class ItemsTableViewController: UITableViewController {
+    
+    var category: String? = nil
+    var itemArray = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        if let category = category {
+            self.navigationItem.title = "\(category) Items"
+        }
+    }
+
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if section == 0 {
+            return itemArray.count
+        }
+        else {
+            return 0
+        }
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath)
+
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item
+
+        return cell
+    }
+    
+
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            itemArray.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let item = itemArray.remove(at: sourceIndexPath.row)
+        itemArray.insert(item, at: destinationIndexPath.row)
+        tableView.reloadData()
+    }
+    
+    
+    @IBAction func addBarButtonPressed(_ sender: UIBarButtonItem) {
+        var alertTextField = UITextField()
+        let alert = UIAlertController(title: "Create New Category", message: "", preferredStyle: .alert)
+        
+        alert.addTextField { (textField) in
+            textField.placeholder = "Name of Category"
+            alertTextField = textField
+        }
+        
+        let action = UIAlertAction(title: "Create", style: .default) { (alertAction) in
+            let text = alertTextField.text!
+            self.itemArray.append(text)
+            self.tableView.reloadData()
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+}
